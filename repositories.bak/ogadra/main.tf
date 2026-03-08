4d813b4353f1b5cc650dd7927b13100341ce0691
ogadra <61941819+ogadra@users.noreply.github.com>
2026-03-08 19:25:58 +0900

    config: add code for ogadra/ogadra

---
+++ repositories.bak/ogadra/main.tf
@@ -0,0 +1,18 @@
+# resource "github_repository" "ogadra" {
+#   name        = "ogadra"
+#   description = "GitHub profile repository for ogadra"
+#   visibility  = "public"
+
+#   has_issues      = true
+#   has_discussions = false
+#   has_projects    = false
+#   has_wiki        = false
+
+#   allow_merge_commit     = true
+#   allow_squash_merge     = false
+#   allow_rebase_merge     = true
+#   allow_auto_merge       = false
+#   delete_branch_on_merge = true
+
+#   vulnerability_alerts = true
+# }
