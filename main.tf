e81740c2f0a7bd1a1bc89ac20b39b09138c6c2c5
ogadra <61941819+ogadra@users.noreply.github.com>
2026-03-08 19:25:58 +0900

    config: add code for ogadra/ogadra

--- main.tf
+++ main.tf
@@ -27,3 +27,24 @@ resource "github_repository" "github" {
 
   vulnerability_alerts = true # 脆弱性アラート有効
 }
+
+resource "github_repository" "ogadra" {
+  name = "ogadra"
+  visibility = "public"
+  topics = [
+    "profile",
+  ]
+
+  has_issues      = true
+  has_discussions = false
+  has_projects    = false
+  has_wiki        = false
+
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = false
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = true
+}
