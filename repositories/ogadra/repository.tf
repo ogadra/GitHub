2dc4597006cb24491d698ea870d619a927f3bcc0
ogadra <61941819+ogadra@users.noreply.github.com>
2026-03-16 01:04:58 +0900

    refactor: split ogadra repository module into separate files

--- repositories/ogadra/repository.tf
+++ repositories/ogadra/repository.tf
@@ -19,15 +19,3 @@ resource "github_repository" "ogadra" {
   vulnerability_alerts = true
 }
 
-resource "github_branch_protection" "ogadra_main" {
-  repository_id = github_repository.ogadra.node_id
-  pattern       = "main"
-
-  allows_force_pushes    = false
-  allows_deletions       = false
-  require_signed_commits = true
-
-  required_pull_request_reviews {
-    required_approving_review_count = 0
-  }
-}
