5ea84464d6f2aefa8c5c06c60f5c46520a02c8c5
ogadra <61941819+ogadra@users.noreply.github.com>
2026-03-15 18:00:17 +0900

    config: add repository secrets management

--- main.tf
+++ main.tf
@@ -42,6 +42,31 @@ resource "github_branch_protection" "github_main" {
   }
 }
 
+data "github_actions_public_key" "github" {
+  repository = github_repository.github.name
+}
+
+data "external" "encrypted_secrets" {
+  program = ["${path.module}/tools/encrypt/encrypt"]
+
+  query = {
+    key  = data.github_actions_public_key.github.key
+    file = "${path.module}/secrets.json"
+  }
+}
+
+resource "github_actions_secret" "github" {
+  for_each = data.external.encrypted_secrets.result
+
+  repository      = github_repository.github.name
+  secret_name     = each.key
+  encrypted_value = each.value
+
+  lifecycle {
+    ignore_changes = [encrypted_value]
+  }
+}
+
 resource "github_repository" "ogadra" {
   name       = "ogadra"
   visibility = "public"
