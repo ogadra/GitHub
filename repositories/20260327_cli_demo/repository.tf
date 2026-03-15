70c398f6151a74187c8964e5e74e1c8cedc71427
ogadra <61941819+ogadra@users.noreply.github.com>
2026-03-16 01:23:53 +0900

    feat: create repo

---
+++ repositories/20260327_cli_demo/repository.tf
@@ -0,0 +1,20 @@
+resource "github_repository" "cli_demo_20260327" {
+  name       = "20260327-cli-demo"
+  visibility = "public"
+  topics = [
+    "terraform",
+  ]
+
+  has_issues      = true
+  has_discussions = false
+  has_projects    = false
+  has_wiki        = false
+
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = false
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = true
+}
