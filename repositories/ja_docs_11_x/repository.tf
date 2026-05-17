84cd92b252f9ad951eae58e2b0cd4c33c633bf1f
ogadra <61941819+ogadra@users.noreply.github.com>
2026-05-18 02:35:03 +0900

    feat: import ja-docs-11.x into terraform

---
+++ repositories/ja_docs_11_x/repository.tf
@@ -0,0 +1,23 @@
+resource "github_repository" "ja_docs_11_x" {
+  name        = "ja-docs-11.x"
+  description = "https://github.com/laravel-ja/ja-docs-11.x"
+  visibility  = "public"
+  topics      = []
+
+  fork         = true
+  source_owner = "laravel-ja"
+  source_repo  = "ja-docs-11.x"
+
+  has_issues      = true
+  has_discussions = false
+  has_projects    = false
+  has_wiki        = false
+
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = false
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = true
+}
