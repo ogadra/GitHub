b48a0eee110b4702bc7c4c2d8f71b997bb017f17
ogadra <61941819+ogadra@users.noreply.github.com>
2026-05-18 02:26:17 +0900

    feat: import slide into terraform

--- repositories/main.tf
+++ repositories/main.tf
@@ -1,25 +1,29 @@
-module "github" {
-  source = "./github"
-}
-
-module "ogadra" {
-  source = "./ogadra"
+module "bunshin" {
+  source = "./bunshin"
 }
 
 module "cli_demo_20260327" {
   source = "./20260327_cli_demo"
 }
 
-module "bunshin" {
-  source = "./bunshin"
+module "github" {
+  source = "./github"
+}
+
+module "nix_claude_code" {
+  source = "./nix_claude_code"
 }
 
 module "nix_takt" {
   source = "./nix_takt"
 }
 
-module "nix_claude_code" {
-  source = "./nix_claude_code"
+module "ogadra" {
+  source = "./ogadra"
+}
+
+module "slide" {
+  source = "./slide"
 }
 
 module "takt" {
---
+++ repositories/slide/repository.tf
@@ -0,0 +1,19 @@
+resource "github_repository" "slide" {
+  name        = "slide"
+  description = "ogadra's slides website."
+  visibility  = "public"
+  topics      = []
+
+  has_issues      = true
+  has_discussions = false
+  has_projects    = false
+  has_wiki        = false
+
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = false
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = true
+}
