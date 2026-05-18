45dfb82268c5be82c6945f28f1b2faaf2a825264
ogadra <61941819+ogadra@users.noreply.github.com>
2026-05-19 00:29:03 +0900

    feat: import siro53.com into terraform

---
+++ repositories/siro53_com/repository.tf
@@ -0,0 +1,23 @@
+resource "github_repository" "siro53_com" {
+  name        = "siro53.com"
+  description = "ホームページ"
+  visibility  = "public"
+  topics      = []
+
+  fork         = true
+  source_owner = "siro53"
+  source_repo  = "siro53.com"
+
+  has_issues      = false
+  has_discussions = false
+  has_projects    = false
+  has_wiki        = false
+
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = false
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = true
+}
