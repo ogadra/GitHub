97ec97804b170cee00dece7f9340ddb7238fdfc5
ogadra <61941819+ogadra@users.noreply.github.com>
2026-05-19 00:21:47 +0900

    feat: import GoogleCalendarExtention into terraform

---
+++ repositories/google_calendar_extention/repository.tf
@@ -0,0 +1,19 @@
+resource "github_repository" "google_calendar_extention" {
+  name        = "GoogleCalendarExtention"
+  description = ""
+  visibility  = "public"
+  topics      = []
+
+  has_issues      = true
+  has_discussions = false
+  has_projects    = false
+  has_wiki        = false
+
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = false
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = true
+}
